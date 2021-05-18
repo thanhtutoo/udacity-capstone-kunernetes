a4ea60557f5c72ffe81aa2c1908a045b72647529
Than Htut Oo <thanhtutoo.dev@gmail.com>
2021-05-19 00:54:25 +0800

    update scripts

--- scripts/deploy_blue.sh
+++ scripts/deploy_blue.sh
@@ -3,8 +3,11 @@
 ## create cluster using eksctl cmd
 
 # Step 1:
-# Build image and add a descriptive tag
-eksctl create cluster --name udacitycluster --nodegroup-name standard-workers --node-type t2.small --nodes 2 --nodes-min 1 --nodes-max 3 --node-ami auto --region us-east-1
-# Step 2: 
-# update kubeconfig
-aws eks --region us-east-1 update-kubeconfig --name udacitycluster
+# set context
+kubectl config use-context arn:aws:eks:us-east-1:243857932979:cluster/udacitycluster
+# Step 2:
+#  apply controller
+kubectl apply -f blue_deploy/blue-controller.json
+# Step 3: 
+# apply service
+kubectl apply -f blue_deploy/blue-service.json
---
+++ scripts/deploy_green.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## create cluster using eksctl cmd
+# Step 1:
+# set context
+kubectl config use-context arn:aws:eks:us-east-1:243857932979:cluster/udacitycluster
+# Step 2:
+#  apply controller
+kubectl apply -f green_deploy/green-controller.json
+# Step 3: 
+# apply service
+kubectl apply -f green_deploy/green-service.json
